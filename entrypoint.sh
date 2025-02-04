b9581dd2e7573bdb6cdeda018cf2afca0a0b2bd2
Shivam <70581178+shivamkonkar@users.noreply.github.com>
2025-02-04 23:30:04 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 echo "Running parent container: $PARENT_CONTAINER"
 
-# Create child containers
-for i in {1..3}; do
+# Create child containers dynamically using a loop
+for i in 1 2 3; do
   docker run --rm --name "${PARENT_CONTAINER}_child_$i" alpine:latest /bin/sh -c "echo 'helloworldfrom${PARENT_CONTAINER}'"
 done
